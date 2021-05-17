2dd92183427fbcecdb7d6a67c28496913de90bb4
AbrahamJuarez <ajuarezdelacruz93@gmail.com>
2021-05-17 18:28:43 -0500

    Nombre del Proyecto: facturación Descripción y/o Comentarios: Primer Commit.

---
+++ src/test/java/com/luca/pacioli/web/app/SpringFramework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luca.pacioli.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
