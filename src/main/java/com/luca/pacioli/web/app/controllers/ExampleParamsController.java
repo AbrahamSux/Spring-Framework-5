8318ef53dad419112ee2c860ff24e80cca7d9c09
Abraham Juárez <AjSux@outlook.com>
2021-05-18 13:27:17 -0500

    Nombre del Proyecto: Facturación.
    Descripción y/o Comentarios: Se agregan métodos para el envío de más de un parámetro de la vista al controlador, y a través de la URL.

--- src/main/java/com/luca/pacioli/web/app/controllers/ExampleParamsController.java
+++ src/main/java/com/luca/pacioli/web/app/controllers/ExampleParamsController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigInteger;
+
 /**
  * @description Clase Controladora para validación del funcionamiento de Params.
  *
  * @author Abraham Juárez de la Cruz - ajuarezdelacruz93@gmail.com
  * @creationDate 18/05/2021 09:58:00 hrs.
- * @version 0.1
+ * @version 0.2
  */
 @Controller
 @RequestMapping("/params")
@@ -32,24 +35,6 @@ public class ExampleParamsController {
 	
 	
 	// MÉTODOS 
-	
-	/**
-	 * Método encargado de mostrar el valor recibido en la vista de mostrar.
-	 * URL: http://localhost:8086/params/string?text=Abraham
-	 * 
-	 * @param texto Argumento recibido.
-	 * @return view mostrar.
-	 */
-	@GetMapping("/string")
-	public ModelAndView param(@RequestParam(name = "text", required = false, defaultValue = "NULL") String texto, ModelAndView mv) {
-		LOGGER.info(">>> param( " + ((texto!=null) ? texto : "NULL") + " )");
-
-		mv.setViewName("params/mostrar");
-		mv.addObject("encabezado", "Parámetro Recibido del Request HTTP GET - URL");
-		mv.addObject("argument", "El valor recibido fue: " + texto);
-		
-		return mv;
-	}
 
 	/**
 	 * Método encargado de re-direccionar a la vista de inicio del folder 'params'.
@@ -63,6 +48,71 @@ public class ExampleParamsController {
 		mm.addAttribute("encabezado", "Envío de Parámetros por Request HTTP GET - URL");
 
 		return "params/index";
+	}
+
+	/**
+	 * Método encargado de mostrar el valor recibido en la vista de mostrar.
+	 * URL: http://localhost:8086/params/value-param?text=Abraham
+	 * 
+	 * @param texto Argumento recibido.
+	 * @return view mostrar.
+	 */
+	@GetMapping("/value-param")
+	public ModelAndView param(@RequestParam(name = "text", required = false, defaultValue = "NULL") String texto, ModelAndView mv) {
+		LOGGER.info(">>> param( " + ((texto!=null) ? texto : "NULL") + " )");
+
+		// SE ESTABLECE LA VISTA A DIRECCIONAR.
+		mv.setViewName("params/mostrar");
+
+		mv.addObject("encabezado", "Parámetro Recibido del Request HTTP GET - URL");
+		mv.addObject("argument", "El valor recibido fue: " + texto);
+		
+		return mv;
+	}
+
+	/**
+	 * Método encargado de mostrar los valores recibidos en la vista de mostrar.
+	 * URL: http://localhost:8086/params/mix-params?name=Ernesto Juárez&number=5517639196
+	 *
+	 * @param nombre Nombre del usuario.
+	 * @param numero Número del usuario.
+	 * @return view mostrar.
+	 */
+	@GetMapping("/mix-params")
+	public ModelAndView param(@RequestParam(name = "name", required = false, defaultValue = "NULL") String nombre,
+							  @RequestParam(name = "number", required = false) BigInteger numero,
+							  ModelAndView mv) {
+		LOGGER.info(">>> param( " + nombre + ", " + ((numero!=null) ? numero : "NULL") + " )");
+
+		// SE ESTABLECE LA VISTA A DIRECCIONAR.
+		mv.setViewName("params/mostrar");
+
+		mv.addObject("encabezado", "Parámetro Recibido del Request HTTP GET - URL");
+		mv.addObject("argument", "El número de teléfono de '" + nombre + "' es : " + numero);
+
+		return mv;
+	}
+
+	/**
+	 * Método encargado de mostrar los valores recibidos en la vista de mostrar.
+	 * URL: http://localhost:8086/params/mix-params-request?name=Ernesto Juárez&number=
+	 *
+	 * @param request Solicitud recibida.
+	 * @return view mostrar.
+	 */
+	@GetMapping("/mix-params-request")
+	public ModelAndView param(HttpServletRequest request, ModelAndView mv) {
+		LOGGER.info(">>> param( " + request.getParameter("name") + ", " + request.getParameter("number") + " )");
+
+		// SE ESTABLECE LA VISTA A DIRECCIONAR.
+		mv.setViewName("params/mostrar");
+
+		mv.addObject("encabezado", "Parámetro Recibido del Request HTTP GET - URL");
+		mv.addObject("argument", "El número de teléfono de '"
+				+ request.getParameter("name") + "' es : "
+				+ ((request.getParameter("number").isEmpty()) ? "0" : request.getParameter("number")) );
+
+		return mv;
 	}
 
 }
