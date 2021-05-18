0c8a03bd33b8dad5be121479e968ee3857d3b46c
Abraham Juárez <AjSux@outlook.com>
2021-05-18 16:42:44 -0500

    Nombre del Proyecto: Facturación.
    Descripción y/o Comentarios: Se agrega el controlador PathVariable, esto para probar la anotación @PathVariable, así como también se realizan las vistas para el paso de uno o más de parámetros.

---
+++ src/main/java/com/luca/pacioli/web/app/controllers/ExamplePathVariableController.java
@@ -0,0 +1,95 @@
+/*
+ * Proyecto:        Facturación - WebApp del Sistema de Facturación
+ * Abraham Juárez S.A.P.I. de C.V. – Todos los derechos reservados. Para uso exclusivo de Abraham Juárez de la Cruz.
+ */
+package com.luca.pacioli.web.app.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigInteger;
+
+/**
+ * @description Clase Controladora para validación del funcionamiento de @PathVariable.
+ *
+ * @author Abraham Juárez de la Cruz - ajuarezdelacruz93@gmail.com
+ * @creationDate 18/05/2021 13:30:00 hrs.
+ * @version 0.1
+ */
+@Controller
+@RequestMapping("/path")
+public class ExamplePathVariableController {
+
+    /**
+     * Utilizada para manipular los mensajes informativos y de error.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExamplePathVariableController.class);
+
+
+
+    // METODOS
+
+    /**
+     * Método encargado de re-direccionar a la vista de inicio del folder 'variables'.
+     *
+     * @return view index.
+     */
+    @GetMapping("/")
+    public String index(ModelMap mm) {
+        LOGGER.info(">>> index()");
+
+        mm.addAttribute("encabezado", "Envío de Parámetros por @PathVariable");
+
+        return "variables/index";
+    }
+
+    /**
+     * Método encargado e re-direccionar a la vista mostrar para visualizar el parámetro recibido.
+     * URL: http://localhost:8086/path/variable/Hola M Supernova
+     *
+     * @param nombreUsuario Nombre del Usuario.
+     * @return view mostrar.
+     */
+    @GetMapping("/variable/{username}")
+    public ModelAndView variable(@PathVariable(name = "username") String nombreUsuario, ModelAndView mv) {
+        LOGGER.info(">>> variable( " + ((nombreUsuario != null) ? nombreUsuario : "NULL") + " )");
+
+        // SE ESTABLECE LA VISTA A DIRECCIONAR.
+        mv.setViewName("variables/mostrar");
+
+        mv.addObject("encabezado", "Recibir Parámetros de la ruta @PathVariable");
+        mv.addObject("user", "El Nombre de Usuario recibido fue: " + nombreUsuario);
+
+        return mv;
+    }
+
+    /**
+     * Método encargado e re-direccionar a la vista mostrar para visualizar los parámetros recibidos.
+     * URL: http://localhost:8086/path/variables/Ernesto Juárez/5517639196
+     *
+     * @param nombreUsuario Nombre del Usuario.
+     * @return view mostrar.
+     */
+    @GetMapping("/variables/{username}/{number}")
+    public ModelAndView variables(@PathVariable(name = "username") String nombreUsuario,
+                                 @PathVariable(name = "number") BigInteger numero,
+                                 ModelAndView mv) {
+        LOGGER.info(">>> variables( " + nombreUsuario + ", " + numero + " )");
+
+        // SE ESTABLECE LA VISTA A DIRECCIONAR.
+        mv.setViewName("variables/mostrar");
+
+        mv.addObject("encabezado", "Recibir Parámetros de la ruta @PathVariable");
+        mv.addObject("user", "El número de telefono del usuario '"
+                + nombreUsuario +"' es: " + numero );
+
+        return mv;
+    }
+
+}
