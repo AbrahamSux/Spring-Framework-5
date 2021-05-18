e7630ff97b27c8dd5b52f494043572d5242b8cbd
Abraham Juárez <AjSux@outlook.com>
2021-05-17 22:56:04 -0500

    Nombre del Proyecto: Facturación
    Descripción y/o Comentarios: Se agrega el Modelo Usuario, así como las vistas para mostrar un usuario y una lista de usuarios establecidos.

--- src/main/java/com/luca/pacioli/web/app/controllers/IndexController.java
+++ src/main/java/com/luca/pacioli/web/app/controllers/IndexController.java
@@ -4,6 +4,9 @@
  */
 package com.luca.pacioli.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,12 +14,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.luca.pacioli.web.app.models.Usuario;
+
 /**
  * @description Clase Controladora para la página de inicio de la aplicación.
  *
  * @author Abraham Juárez de la Cruz - ajuarezdelacruz93@gmail.com
  * @creationDate 17/05/2021 19:34:00 hrs.
- * @version 0.1
+ * @version 0.5
  */
 @Controller
 @RequestMapping("/facturacion")
@@ -32,9 +37,9 @@ public class IndexController {
 	// METODOS
 	
 	/**
-	 * Método encargado de redireccionar a la vista principal.
+	 * Método encargado de re-direccionar a la vista principal.
 	 * 
-	 * @return view
+	 * @return view index.
 	 */
 	@GetMapping({"/index", "/", "/home"})
 	public String index(Model model) {
@@ -44,6 +49,64 @@ public class IndexController {
 		model.addAttribute("mensajeBienvenida", "¡Bienvenido al Sistema de Facturación!");
 		
 		return "index";
+	}
+	
+	/**
+	 * Método encargado de re-direccionar a la vista del perfil.
+	 * 
+	 * @return view perfil.
+	 */
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		LOGGER.info(">>> perfil()");
+		
+		Usuario usuario = new Usuario("Ernesto", "Juárez", "ernesto.juarez@gmail.com");
+		
+		model.addAttribute("encabezado", "Perfil del Usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("user", usuario);
+		
+		LOGGER.info("Datos Establecidos: {}", ((usuario != null) ? usuario.toString() : "NULL") );
+		
+		return "perfil";
+	}
+	
+	/**
+	 * Método encargado de re-direccionar a la vista de listar.
+	 * 
+	 * @return view listar.
+	 */
+	@GetMapping("/listar")
+	public String listar(Model model) {
+		LOGGER.info(">>> listar()");
+		
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios = getLista();
+		
+		LOGGER.info("Datos Establecidos: {}", ((usuarios != null) ? usuarios.toString() : "NULL") );
+		
+		model.addAttribute("encabezado", "Lista de Usuarios");
+		model.addAttribute("users", usuarios);
+		
+		return "listar";
+	}
+	
+	
+	
+	// METODOS PRIVADOS
+	
+	/**
+	 * Método auxiliar encargado de establecer una lista de usuarios.
+	 * 
+	 * @return una lista de Usuarios.
+	 */
+	private List<Usuario> getLista() {
+		List<Usuario> users = new ArrayList<>();
+		users.add(new Usuario("Ernesto", "Juárez", null));
+		users.add(new Usuario("Dalia", "de la Cruz", "dalia.delacruz@outlook.com"));
+		users.add(new Usuario("Diana", "Camarillo", "diana.camarillo@gmail.com"));
+		users.add(new Usuario("José", "Madero", "jose.madero@hotmail.com"));
+		
+		return users;
 	}
 
 }
