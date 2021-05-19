275af9c03e2313b6bcbf93585ac969c180a737e5
Abraham Juárez <AjSux@outlook.com>
2021-05-19 12:27:40 -0500

    Nombre del Proyecto: Facturación.
    Descripción y/o Comentarios: Se agrega otra implementación al mismo servicio, y se establece el Impl que será por default (@Primary) en nuestras clases que utilicen este servicio.

--- src/main/java/com/luca/pacioli/web/app/services/impl/ComplejoServiceImpl.java
+++ src/main/java/com/luca/pacioli/web/app/services/impl/ComplejoServiceImpl.java
@@ -7,22 +7,24 @@ package com.luca.pacioli.web.app.services.impl;
 import com.luca.pacioli.web.app.services.IndexService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 /**
  * @description Clase que Implementa la lógica del Servicio {@link IndexService} utilizada para el acceso a datos.
  *
  * @author Abraham Juárez de la Cruz - ajuarezdelacruz93@gmail.com
- * @creationDate 19/05/2021 10:23:00 hrs.
+ * @creationDate 19/05/2021 12:17:00 hrs.
  * @version 0.1
  */
-@Service("indexService")
-public class IndexServiceImpl implements IndexService {
+@Service("complejoService")
+@Primary
+public class ComplejoServiceImpl implements IndexService {
 
     /**
      * Utilizada para manipular los mensajes informativos y de error.
      */
-    private static final Logger LOGGER = LoggerFactory.getLogger(IndexServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComplejoServiceImpl.class);
 
 
 
@@ -32,7 +34,7 @@ public class IndexServiceImpl implements IndexService {
     public String testMessage() {
         LOGGER.info(">>> test() ");
 
-        return "Mensaje enviado desde el metodo test() de IndexService...";
+        return "Mensaje enviado desde el metodo test() de ComplejoServiceImpl...";
     }
 
 }
